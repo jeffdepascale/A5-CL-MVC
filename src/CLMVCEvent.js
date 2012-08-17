aa012f896195774acf4082e78df0034d94ffc68b
Jeff dePascale <jdepascale@testpilot111.com>
2012-08-17 16:33:06 -0400

    added MVCEvent

---
+++ src/CLMVCEvent.js
@@ -0,0 +1,32 @@
+
+a5.Package('a5.cl.mvc')
+
+	.Extends('a5.Event')
+	.Static(function(CLMVCEvent){
+				
+		/**
+		 * @event
+		 * @description Dispatched when the render() method is called on a mappable controller.
+		 * @param {a5.cl.CLController} controller
+		 */
+		CLMVCEvent.RENDER_CONTROLLER = 'renderController';
+		
+		/**
+		 * @event
+		 * @description Dispatched by CLViews when they are added to a parent view.  This event is useful for detecting when children are added to a specific branch of the view tree.
+		 */
+		CLMVCEvent.ADDED_TO_PARENT = 'addedToParent';
+		
+		/**
+		 * @event
+		 * @description Dispatched by CLViews when they are added to a parent view.  This event is useful for detecting when children are added to a specific branch of the view tree.
+		 */
+		CLMVCEvent.REMOVED_FROM_PARENT = 'removedFromParent';
+		
+	})
+	.Class('CLMVCEvent', function(cls, im){
+		
+		cls.CLMVCEvent = function(){
+			cls.superclass(this);
+		}		
+})
